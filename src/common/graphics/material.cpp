c3b59aac328d02c3e08baf76a9845d63aee867bc
WireWhiz <43615314+WireWhiz@users.noreply.github.com>
2022-11-07 21:21:01 -0700

    Enable back-face culling

--- src/common/graphics/material.cpp
+++ src/common/graphics/material.cpp
@@ -274,8 +274,8 @@ namespace graphics
         rasterizer.rasterizerDiscardEnable = VK_FALSE;
         rasterizer.polygonMode = VK_POLYGON_MODE_FILL;
         rasterizer.lineWidth = 1.0f;
-        rasterizer.cullMode = VK_CULL_MODE_NONE; //VK_CULL_MODE_BACK_BIT;
-        rasterizer.frontFace = VK_FRONT_FACE_COUNTER_CLOCKWISE;
+        rasterizer.cullMode = VK_CULL_MODE_BACK_BIT;
+        rasterizer.frontFace = VK_FRONT_FACE_CLOCKWISE;
         rasterizer.depthBiasEnable = VK_FALSE;
         rasterizer.depthBiasConstantFactor = 0.0f;
         rasterizer.depthBiasClamp = 0.0f;
