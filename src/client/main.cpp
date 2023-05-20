3fb616b749ba17c92c7a5af13c3eda52f3152f97
rgnter <maros.prejsa1@gmail.com>
2023-05-20 13:58:42 +0200

    fix client

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -1,36 +1,39 @@
+#include "client.h"
 
-//#include "assets/assetManager.h"
-//#include "chunk/chunkLoader.h"
-//#include "client.h"
-//#include "ecs/entity.h"
-//#include "graphics/graphics.h"
-//#include "systems/transforms.h"
-//#include <iostream>
+#include <config/config.h>
+#include <networking/networking.h>
+#include <assets/assetManager.h>
+#include <fileManager/fileManager.h>
+#include <runtimeServer/runtimeServer.h>
+
+#include <chunk/chunkLoader.h>
+#include <systems/transforms.h>
+#include <graphics/graphics.h>
 
 int main()
 {
 
-//    Config::loadConfig();
-//    std::cout << "BraneSurfer starting up\n";
-//
-//    Runtime::init();
-//    Timeline& tl = Runtime::timeline();
-//    tl.addBlock("asset management");
-//    tl.addBlock("networking");
-//    tl.addBlock("before main");
-//    tl.addBlock("main");
-//    tl.addBlock("draw");
-//    Runtime::addModule<FileManager>();
-//    Runtime::addModule<NetworkManager>();
-//    Runtime::addModule<EntityManager>();
-//    Runtime::addModule<AssetManager>();
-//    Runtime::addModule<ChunkLoader>();
-//    Runtime::addModule<graphics::VulkanRuntime>();
-//    Runtime::addModule<Transforms>();
-//    Runtime::addModule<Client>();
-//
-//    Runtime::run();
-//
-//    Runtime::cleanup();
+    Config::loadConfig();
+    std::cout << "BraneSurfer starting up\n";
+
+    Runtime::init();
+    Timeline& tl = Runtime::timeline();
+    tl.addBlock("asset management");
+    tl.addBlock("networking");
+    tl.addBlock("before main");
+    tl.addBlock("main");
+    tl.addBlock("draw");
+    Runtime::addModule<FileManager>();
+    Runtime::addModule<NetworkManager>();
+    Runtime::addModule<EntityManager>();
+    Runtime::addModule<AssetManager>();
+    Runtime::addModule<ChunkLoader>();
+    Runtime::addModule<graphics::VulkanRuntime>();
+    Runtime::addModule<Transforms>();
+   Runtime::addModule<Client>();
+
+    Runtime::run();
+
+    Runtime::cleanup();
     return 0;
 }
